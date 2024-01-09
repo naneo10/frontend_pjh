36dc2de4f05c86979ce4dbcfffc41678a8070701
naneo10 <fenerte10@gmail.com>
2024-01-09 17:48:24 +0900

    20240109

---
+++ E_react0912/src/views/0109/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Axios01 from './a_Axios/Axios01';
+import Axios02 from './a_Axios/Axios02';
+import Axios03 from './a_Axios/Axios03';
+
+export default function Index() {
+  return(
+    <div>
+      <h1>0109 리액트 수업자료</h1>
+      <h2>Axios</h2>
+      {/* 
+        <Axios01 />
+        <Axios02 /> 
+      */}
+      <Axios03 />
+    </div>
+  )
+}
