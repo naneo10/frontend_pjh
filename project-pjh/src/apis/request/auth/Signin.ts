96a501f443b33774c84ba7b2d5257ad5bdb99a14
naneo10 <fenerte10@gmail.com>
2024-01-19 17:43:28 +0900

    20240119

---
+++ project-pjh/src/apis/request/auth/Signin.ts
@@ -0,0 +1,14 @@
+export interface SignInRequest {
+  email: string;
+  password: string;
+}
+
+export interface SignUpRequest {
+  email: string;
+  password: string;
+  nickname: string;
+  telNumber: string;
+  address: string;
+}
+
+export default SignInRequest;
